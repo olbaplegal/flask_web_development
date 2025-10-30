9f76e3bf374d1346e75768ed7b70e6227b73070c
olbap <olbaplegal@gmail.com>
2025-10-30 10:46:14 -0300

    implementado app.py

---
+++ 010_login/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from controllers import login_controller
+
+app = Flask(__name__, template_folder='views/templates', static_folder='views/static')
+
+# Carrega as informações do arquivo config.py
+# Carrega as informações da SECRET_KEY e DB_CONFIG para dentro do objeto de configuração da aplicação
+app.config.from_object('config.Confic')
+
+login_controller.configura_rotas(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
