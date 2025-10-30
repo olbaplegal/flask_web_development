9991a84f02d3f5456b022a51795e31cc371fdc1a
olbap <olbaplegal@gmail.com>
2025-10-30 10:41:26 -0300

    implementado config.py

---
+++ 010_login/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = 'Professor123'
+    DB_CONFIG = {
+        'host':'localhost',
+        'user':'root',
+        'password':'password',
+        'database':'login_usuarios'
+    }
